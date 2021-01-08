d84036c72adc655d0136a019e183887772b1b2c9
Thanasis Karanikolas <sdi1700045@di.uoa.gr>
2021-01-08 17:17:08 +0200

    Reformatting code

--- user-authorizer/src/main/java/com/karliatr/userauthorizer/models/User.java
+++ user-authorizer/src/main/java/com/karliatr/userauthorizer/models/User.java
@@ -1,24 +1,21 @@
 package com.karliatr.userauthorizer.models;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(	name = "users",
+@Table(name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
         })
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank
@@ -35,7 +32,7 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(	name = "user_roles",
+    @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
